de43ac1e006ca4c548fcfa91daabc5b5da3a0eec
paul449 <paul_bilbatua@hotmail.com>
2024-06-11 11:15:40 -0500

    conditional rendering

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,33 +1,33 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import components from '../src/components'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='wrapper'>
+      <div className='header'>
+        <ul>
+          if('/AboutMe'=== AboutMe){
+            content = <li><AboutMe/></li>
+          } else if('/Portfolio' === Portfolio){
+            content = <li><Portfolio/></li>
+          } else if('/Contact' === Contact){
+            content = <li><Contact/></li>
+          } else{
+            content = <li><Resume/></li>
+          }
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='content'>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='footer'>
+
+      </div>
+    </div>
     </>
   )
 }
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react' //importing react
+import ReactDOM from 'react-dom/client' // import client from ReactDOM
 import App from './App.jsx'
 import './index.css'
 
